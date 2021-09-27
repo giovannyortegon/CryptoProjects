485047c24ac6385c748fb5490a754a85ba538f8b
giovannyortegon <ortegon.giovanny@hotmail.com>
2021-09-26 23:16:17 -0500

    [ADD] transposition File cipher

--- Hacking Secret Ciphers/Chap11/transpositionFileCipher.py
+++ Hacking Secret Ciphers/Chap11/transpositionFileCipher.py
@@ -14,7 +14,7 @@ def main():
 
 	if not os.path.exists(inputFilename):
 		print("The file %s does not exist. Quitting..." % inputFilename)
-		sys.exit(1)
+		sys.exit()
 
 	if os.path.exists(outputFilename):
 		print("This will overwrite the file %s. (C)ontinue or (Q)uit?" %outputFilename)
@@ -22,28 +22,28 @@ def main():
 		if not response.lower().startswith("c"):
 			sys.exit()
 
-		fileObj = open(inputFilename)
-		content = fileObj.read()
-		fileObj.close()
+	fileObj = open(inputFilename)
+	content = fileObj.read()
+	fileObj.close()
 
-		print("%sing..." %(myMode.title()))
+	print("%sing..." %(myMode.title()))
 
-		startTime = time.time()
+	startTime = time.time()
 
-		if myMode == 'encrypt':
-			translated = transpositionEncrypt.encryptMessage(myKey, content)
-		elif myMode == 'decrypt':
-			translated = transpositionDecrypt.decryptMessage(myKey, content)
+	if myMode == 'encrypt':
+		translated = transpositionEncrypt.encryptMessage(myKey, content)
+	elif myMode == 'decrypt':
+		translated = transpositionDecrypt.decryptMessage(myKey, content)
 
-		totalTime = round(time.time() - startTime, 2)
-		print("%sion time: %s seconds" %(myMode.title(), totalTime))
+	totalTime = round(time.time() - startTime, 2)
+	print("%sion time: %s seconds" %(myMode.title(), totalTime))
 
-		outputFileObj = open(outputFilename, 'w')
-		outputFileObj.write(translated)
-		outputFileObj.close()
+	outputFileObj = open(outputFilename, 'w')
+	outputFileObj.write(translated)
+	outputFileObj.close()
 
-		print("Done %sing %s (%s characters)." %(myMode, inputFilename, len(content)))
-		print("$sed file is %s." %(myMode.title(), outputFilename))
+	print("Done %sing %s (%d characters)." %(myMode, inputFilename, len(content)))
+	print("%sed file is %s." %(myMode.title(), outputFilename))
 
 
 if __name__ == "__main__":
